066093a7a8b7af603ebcd75a02681391e723dd85
Alexander Mikhailov <grey-machine@yandex.ru>
2022-06-19 13:43:00 +0300

    Replaced Comment with Docstring

--- plots_un_usa.py
+++ plots_un_usa.py
@@ -12,9 +12,14 @@ import requests
 
 
 def main():
-    # =========================================================================
-    # Purpose: Visualize the U.S. Contribution to World`s Gross Domestic Product (GDP)
-    # =========================================================================
+    '''
+    Visualize the U.S. Contribution to World`s Gross Domestic Product (GDP)
+
+    Returns
+    -------
+    None.
+
+    '''
     URL = 'https://unstats.un.org/unsd/amaapi/api/file/2'
     df = pd.read_excel(io.BytesIO(requests.get(URL).content), skiprows=2)
     df = df[df.iloc[:, 2] == 'Gross Domestic Product (GDP)']
