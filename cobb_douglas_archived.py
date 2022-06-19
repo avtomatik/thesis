cf6ff9f704143b6b0ca88c9cac9d289858620e05
Alexander Mikhailov <grey-machine@yandex.ru>
2022-06-19 13:37:25 +0300

    Added Entry Point

--- cobb_douglas_archived.py
+++ cobb_douglas_archived.py
@@ -14,79 +14,99 @@ from prepare.lib import get_data_capital_purchases
 from plot.lib import plot_capital_purchases
 from plot.lib import plot_cobb_douglas_alt
 from prepare.lib import transform_cobb_douglas
+from prepare.lib import transform_cobb_douglas_alt
 from toolkit.lib import calculate_curve_fit_params
 from prepare.lib import get_data_cobb_douglas
 
 
-FOLDER = '/media/alexander/321B-6A94'
-os.chdir(FOLDER)
-
-
 print(__doc__)
-# =============================================================================
-# Project I. Classified
-# =============================================================================
-df = get_data_cobb_douglas()
-# result_frame_b = df.iloc[:, [0, 1, 2, 4]]
-plot_cobb_douglas_alt(df)
-plot_cobb_douglas_alt(result_frame_b)
 
-result_frame_a = df.iloc[:, [0, 1, 2]]
-result_frame_b = df.iloc[:, [0, 1, 3]]
-result_frame_c = df.iloc[:, [0, 1, 4]]
-result_frame_d, result_frame_e = get_data_version_a()
-result_frame_f, result_frame_g, result_frame_h = get_data_version_b()
-result_frame_i = dataset_version_c()
-calculate_curve_fit_params(result_frame_a)
-calculate_curve_fit_params(result_frame_b)
-calculate_curve_fit_params(result_frame_c)
-calculate_curve_fit_params(result_frame_d)
-calculate_curve_fit_params(result_frame_e)
-calculate_curve_fit_params(result_frame_f)
-calculate_curve_fit_params(result_frame_g)
-calculate_curve_fit_params(result_frame_h)
-calculate_curve_fit_params(result_frame_i)
-calculate_curve_fit_params(result_frame_a)
-calculate_curve_fit_params(result_frame_b)
-calculate_curve_fit_params(result_frame_c)
-# =============================================================================
-# No Capacity Utilization Adjustment
-# =============================================================================
-calculate_curve_fit_params(result_frame_d)
-# =============================================================================
-# Capacity Utilization Adjustment
-# =============================================================================
-calculate_curve_fit_params(result_frame_e)
-# =============================================================================
-# Option: 1929--2013, No Capacity Utilization Adjustment
-# =============================================================================
-calculate_curve_fit_params(result_frame_f)
-# =============================================================================
-# Option: 1967--2013, No Capacity Utilization Adjustment
-# =============================================================================
-calculate_curve_fit_params(result_frame_g)
-# =============================================================================
-# Option: 1967--2012, Capacity Utilization Adjustment
-# =============================================================================
-calculate_curve_fit_params(result_frame_h)
-calculate_curve_fit_params(result_frame_i)
-# =============================================================================
-# Project II. Scipy Signal Median Filter, Non-Linear Low-Pass Filter
-# =============================================================================
-procedure(result_frame_a)
-procedure(result_frame_b)
-procedure(result_frame_c)
-procedure(result_frame_d)
-procedure(result_frame_e)
-procedure(result_frame_f)
-procedure(result_frame_g)
-procedure(result_frame_h)
-procedure(result_frame_i)
-procedure(result_frame_a)
-procedure(result_frame_b)
-procedure(result_frame_c)
-# =============================================================================
-# Project III. Scipy Signal Wiener Filter
-# =============================================================================
-purchases_frame = get_dataset_capital_purchases()
-plot_capital_purchases(purchases_frame)
+
+def main():
+    FOLDER = '/media/alexander/321B-6A94'
+    FIG_MAP = {
+        'fg_a': 'Chart I Progress in Manufacturing {}$-${} ({}=100)',
+        'fg_b': 'Chart II Theoretical and Actual Curves of Production {}$-${} ({}=100)',
+        'fg_c': 'Chart III Percentage Deviations of $P$ and $P\'$ from Their Trend Lines\nTrend Lines=3 Year Moving Average',
+        'fg_d': 'Chart IV Percentage Deviations of Computed from Actual Product {}$-${}',
+        'fg_e': 'Chart V Relative Final Productivities of Labor and Capital',
+        'year_price': 1899,
+    }
+    os.chdir(FOLDER)
+    # =============================================================================
+    # Project I. Classified
+    # =============================================================================
+    df = get_data_cobb_douglas(5)
+    _df_b = df.iloc[:, [0, 1, 3]]
+    plot_cobb_douglas_alt(
+        *transform_cobb_douglas_alt(df),
+        FIG_MAP
+    )
+    plot_cobb_douglas_alt(
+        *transform_cobb_douglas_alt(_df_b),
+        FIG_MAP
+        )
+
+    _df_a = df.iloc[:, [0, 1, 2]]
+    _df_b = df.iloc[:, [0, 1, 3]]
+    _df_c = df.iloc[:, [0, 1, 4]]
+    _df_d, _df_e = get_data_version_a()
+    _df_f, _df_g, _df_h = get_data_version_b()
+    _df_i = dataset_version_c()
+    calculate_curve_fit_params(_df_a)
+    calculate_curve_fit_params(_df_b)
+    calculate_curve_fit_params(_df_c)
+    calculate_curve_fit_params(_df_d)
+    calculate_curve_fit_params(_df_e)
+    calculate_curve_fit_params(_df_f)
+    calculate_curve_fit_params(_df_g)
+    calculate_curve_fit_params(_df_h)
+    calculate_curve_fit_params(_df_i)
+    calculate_curve_fit_params(_df_a)
+    calculate_curve_fit_params(_df_b)
+    calculate_curve_fit_params(_df_c)
+    # =============================================================================
+    # No Capacity Utilization Adjustment
+    # =============================================================================
+    calculate_curve_fit_params(_df_d)
+    # =============================================================================
+    # Capacity Utilization Adjustment
+    # =============================================================================
+    calculate_curve_fit_params(_df_e)
+    # =============================================================================
+    # Option: 1929--2013, No Capacity Utilization Adjustment
+    # =============================================================================
+    calculate_curve_fit_params(_df_f)
+    # =============================================================================
+    # Option: 1967--2013, No Capacity Utilization Adjustment
+    # =============================================================================
+    calculate_curve_fit_params(_df_g)
+    # =============================================================================
+    # Option: 1967--2012, Capacity Utilization Adjustment
+    # =============================================================================
+    calculate_curve_fit_params(_df_h)
+    calculate_curve_fit_params(_df_i)
+    # =============================================================================
+    # Project II. Scipy Signal Median Filter, Non-Linear Low-Pass Filter
+    # =============================================================================
+    procedure(_df_a)
+    procedure(_df_b)
+    procedure(_df_c)
+    procedure(_df_d)
+    procedure(_df_e)
+    procedure(_df_f)
+    procedure(_df_g)
+    procedure(_df_h)
+    procedure(_df_i)
+    procedure(_df_a)
+    procedure(_df_b)
+    procedure(_df_c)
+    # =============================================================================
+    # Project III. Scipy Signal Wiener Filter
+    # =============================================================================
+    purchases_frame = get_dataset_capital_purchases()
+    plot_capital_purchases(purchases_frame)
+
+
+if __name__ == '__main__':
+    main()
