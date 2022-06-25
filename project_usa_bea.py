c167b57b87ec5339998535bce497676eefe14ffd
Alexander Mikhailov <grey-machine@yandex.ru>
2022-06-25 22:31:11 +0300

    Minor Style Changes

--- project_usa_bea.py
+++ project_usa_bea.py
@@ -7,13 +7,14 @@ Created on Thu Jan  9 21:17:48 2020
 
 
 import os
-from prepare.lib import get_data_combined
-from prepare.lib import get_data_combined_archived
-from prepare.lib import transform_a
-from prepare.lib import transform_b
-from prepare.lib import transform_c
-from prepare.lib import transform_d
-from prepare.lib import transform_e
+from collect.lib import collect_combined
+from collect.lib import collect_combined_archived
+from collect.lib import transform_a
+from collect.lib import transform_b
+from collect.lib import transform_c
+from collect.lib import transform_d
+from collect.lib import transform_e
+from collect.lib import transform_kurenkov
 from plot.lib import plot_a
 from plot.lib import plot_b
 from plot.lib import plot_c
@@ -24,43 +25,40 @@ from plot.lib import plot_kurenkov
 
 FOLDER = '/media/alexander/321B-6A94'
 os.chdir(FOLDER)
-source_frame_a = get_data_combined_archived()
-source_frame_b = get_data_combined()
+_df_a = collect_combined_archived()
+_df_b = collect_combined()
 # =============================================================================
 # Project: Initial Version Dated: 05 October 2012
 # =============================================================================
-result_frame_a_b = transform_a(source_frame_a)
-result_frame_a_c = transform_a(source_frame_b)
-plot_a(result_frame_a_b)
-plot_a(result_frame_a_c)
+df_a_a = transform_a(_df_a)
+df_a_b = transform_a(_df_b)
+plot_a(df_a_a)
+plot_a(df_a_b)
 # =============================================================================
 # Project: Initial Version Dated: 23 November 2012
 # =============================================================================
-result_frame_b_b = transform_b(source_frame_a)
-result_frame_b_c = transform_b(source_frame_b)
-plot_b(result_frame_b_b)
-plot_b(result_frame_b_c)
+df_b_a = transform_b(_df_a)
+df_b_b = transform_b(_df_b)
+plot_b(df_b_a)
+plot_b(df_b_b)
 # =============================================================================
 # Project: Initial Version Dated: 16 June 2013
 # =============================================================================
-result_frame_c_b = transform_c(source_frame_a)
-result_frame_c_c = transform_c(source_frame_b)
-plot_c(result_frame_c_b)
-plot_c(result_frame_c_c)
+df_c_a = transform_c(_df_a)
+df_c_b = transform_c(_df_b)
+plot_c(df_c_a)
+plot_c(df_c_b)
 # =============================================================================
 # Project: Initial Version Dated: 15 June 2015
 # =============================================================================
-result_frame_d = transform_d(source_frame_b)
-plot_d(result_frame_d)
+plot_d(transform_d(_df_b))
 # =============================================================================
 # Project: Initial Version Dated: 17 February 2013
 # =============================================================================
-result_frame_e_a, result_frame_e_b = transform_e(source_frame_a)
-plot_e(result_frame_e_a)
-plot_e(result_frame_e_b)
+df_e_a, df_e_b = transform_e(_df_a)
+plot_e(df_e_a)
+plot_e(df_e_b)
 # =============================================================================
 # Project: BEA Data Compared with Kurenkov Yu.V. Data
 # =============================================================================
-result_frame_f_a, result_frame_f_b, result_frame_f_c, result_frame_f_d = transform_f(
-    source_frame_a)
-plot_kurenkov(result_frame_f_a, result_frame_f_b, result_frame_f_c, result_frame_f_d)
+plot_kurenkov(transform_kurenkov(_df_a))
--- project_usa_census.py
+++ project_usa_census.py
@@ -7,17 +7,17 @@ Created on Sat Jan 18 00:13:17 2020
 
 
 import os
-from prepare.lib import get_data_census_a
-from prepare.lib import get_data_census_b_a
-from prepare.lib import get_data_census_b_b
-from prepare.lib import get_data_census_c
-from prepare.lib import get_data_census_e
-from prepare.lib import get_data_census_f
-from prepare.lib import get_data_census_g
-from prepare.lib import get_data_census_i_a
-from prepare.lib import get_data_census_i_b
-from prepare.lib import get_data_census_i_c
-from prepare.lib import get_data_census_j
+from collect.lib import collect_census_a
+from collect.lib import collect_census_b_a
+from collect.lib import collect_census_b_b
+from collect.lib import collect_census_c
+from collect.lib import collect_census_e
+from collect.lib import collect_census_f
+from collect.lib import collect_census_g
+from collect.lib import collect_census_i_a
+from collect.lib import collect_census_i_b
+from collect.lib import collect_census_i_c
+from collect.lib import collect_census_j
 from plot.lib import plot_census_a
 from plot.lib import plot_census_b_capital
 from plot.lib import plot_census_b_deflator
@@ -36,38 +36,44 @@ from plot.lib import plot_census_k
 
 
 FOLDER = '/media/alexander/321B-6A94'
-SERIES_IDS = (
-    'P0248', 'P0249', 'P0250', 'P0251', 'P0262', 'P0265', 'P0266', 'P0267',
-    'P0268', 'P0269', 'P0293', 'P0294', 'P0295',
-)
-ALTERNATIVE_IDS = (
-    'P0231', 'P0232', 'P0233', 'P0234', 'P0235', 'P0236', 'P0237', 'P0238',
-    'P0239', 'P0240', 'P0241', 'P0244', 'P0247', 'P0248', 'P0249', 'P0250',
-    'P0251', 'P0252', 'P0253', 'P0254', 'P0255', 'P0256', 'P0257', 'P0258',
-    'P0259', 'P0260', 'P0261', 'P0262', 'P0263', 'P0264', 'P0265', 'P0266',
-    'P0267', 'P0268', 'P0269', 'P0270', 'P0271', 'P0277', 'P0279', 'P0281',
-    'P0282', 'P0283', 'P0284', 'P0286', 'P0288', 'P0290', 'P0293', 'P0294',
-    'P0295', 'P0296', 'P0297', 'P0298', 'P0299', 'P0300',
-)
 
 
 os.chdir(FOLDER)
-plot_census_a(*get_data_census_a())
-plot_census_b_capital(get_data_census_b_a())
-plot_census_b_deflator(get_data_census_b_b())
-plot_census_c(*get_data_census_c())
+plot_census_a(*collect_census_a())
+plot_census_b_capital(collect_census_b_a())
+plot_census_b_deflator(collect_census_b_b())
+plot_census_c(*collect_census_c())
 # =============================================================================
 # Census Production Series
 # =============================================================================
+SERIES_IDS = (
+    'P0248', 'P0249', 'P0250', 'P0251', 'P0262',
+    'P0265', 'P0266', 'P0267', 'P0268', 'P0269',
+    'P0293', 'P0294', 'P0295',
+)
+ids = itertools.chain(
+    range(231, 242),
+    range(244, 245),
+    range(247, 272),
+    range(277, 278),
+    range(279, 280),
+    range(281, 285),
+    range(286, 287),
+    range(288, 289),
+    range(290, 291),
+    range(293, 301),
+)
+SERIES_IDS_ALT = tuple(f'P{_id:04n}' for _id in ids)
+
 plot_census_d(SERIES_IDS)
-plot_census_e(get_data_census_e())
-result_frame = get_data_census_f()
-plot_census_f_a(result_frame)
-plot_census_f_b(result_frame)
-plot_census_g(get_data_census_g())
+plot_census_e(collect_census_e())
+df = collect_census_f()
+plot_census_f_a(df)
+plot_census_f_b(df)
+plot_census_g(collect_census_g())
 plot_census_h()
-plot_census_i_a(get_data_census_i_a())
-plot_census_i_b(get_data_census_i_b())
-plot_census_i_c(get_data_census_i_c())
-plot_census_j(get_data_census_j())
+plot_census_i_a(collect_census_i_a())
+plot_census_i_b(collect_census_i_b())
+plot_census_i_c(collect_census_i_c())
+plot_census_j(collect_census_j())
 plot_census_k()
